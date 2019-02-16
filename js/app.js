4086481cae32e08fc7fb5867c45f95cfceba4d2f
AdrianFernandez13 <adrianfernandezloeza@gmail.com>
2019-02-16 15:37:49 -0600

    able to draw game and 1 immovable enemy

--- js/app.js
+++ js/app.js
@@ -12,24 +12,46 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.posX += this.movSpeed * dt; //It makes the 
+    this.posX += this.movSpeed * dt; //It adds to the initial position of the enemy
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(playerPosX, playerPosY, playerMovSpeed) {
+    this.playerPosX = playerPosX; // where the player will spawn in the x axis
+    this.playerPosY = playerPosY; // where the player will spawn in the y axis
+    this.playerMovSpeed = playerMovSpeed; // player's movement speed
+    this.sprite = "images/char-horn-girl.png"; //the image for the player
+};
+
+Player.prototype.update = function(dt) {
+    this.playerPosX += this.playerMovSpeed * dt;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.playerPosX, this.playerPosY);
+};
+
+/*Player.prototype.handleInput = function() {
+
+}*/
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var player = new Player(0,0, 10);
+var enemy = new Enemy(250, 0, 10);
 
+allEnemies.push(enemy);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
