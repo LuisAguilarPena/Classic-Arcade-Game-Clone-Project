c9d6bdcaba7dce08cd130427fffba71fe6986191
AdrianFernandez13 <adrianfernandezloeza@gmail.com>
2019-02-16 16:03:59 -0600

    enemy moves and player is drawn

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function(posX, posY, movSpeed) { // Variables applied to each of our instances go here,
-    this.posX = posX; // Where the enemy will spawn in the x axis
-    this.posY = posY; // Where the enemy will spawn in the y axis
-    this.movSpeed = movSpeed; // enemies' movement speed
+var Enemy = function(x, y, speed) { // Variables applied to each of our instances go here,
+    this.x = x; // Where the enemy will spawn in the x axis
+    this.y = y; // Where the enemy will spawn in the y axis
+    this.speed = speed; // enemies' movement speed
     this.sprite = 'images/enemy-bug.png'; // The image/sprite for our enemies, this uses a helper we've provided to easily load images
 };
 
@@ -12,44 +12,44 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.posX += this.movSpeed * dt; //It adds to the initial position of the enemy
+    this.x += this.speed * dt; //It adds to the initial position of the enemy
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(playerPosX, playerPosY, playerMovSpeed) {
-    this.playerPosX = playerPosX; // where the player will spawn in the x axis
-    this.playerPosY = playerPosY; // where the player will spawn in the y axis
-    this.playerMovSpeed = playerMovSpeed; // player's movement speed
-    this.sprite = "images/char-horn-girl.png"; //the image for the player
+var Player = function(x, y, speed) {
+    this.x = x; // where the player will spawn in the x axis
+    this.y = y; // where the player will spawn in the y axis
+    this.speed = speed; // player's movement speed
+    this.sprite = 'images/char-boy.png'; //the image for the player
 };
 
 Player.prototype.update = function(dt) {
-    this.playerPosX += this.playerMovSpeed * dt;
+    //this.x += this.speed * dt;
 }
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.playerPosX, this.playerPosY);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-/*Player.prototype.handleInput = function() {
+Player.prototype.handleInput = function(keyPress) {
 
-}*/
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 var allEnemies = [];
-var player = new Player(0,0, 10);
-var enemy = new Enemy(250, 0, 10);
+var player = new Player(0,325, 100);
+var enemy = new Enemy(0, 50, 100);
 
 allEnemies.push(enemy);
 
